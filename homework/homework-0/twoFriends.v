19501b89de7c76ce1d3791d74f7e2023385e046a
Alan Loreto Cornidez <aloretocornidez@arizona.edu>
2024-01-11 15:51:57 -0700

    auto-commit

--- homework/homework-0/twoFriends.v
+++ homework/homework-0/twoFriends.v
@@ -25,6 +25,45 @@ endmodule
 
 
 // Logic done in a structural manner.
+// or gate
+module gate_or (in, out);
+  
+  input in[0:1];
+  output out;
+
+  initial begin
+    output <= 0;
+  end
+
+  always @(*) begin 
+
+    output <= in[0] | in[1];
+
+  end
+
+
+endmodule
+
+// and gate
+module gate_and ( in, out);
+
+  input in[0:1];
+  output out;
+
+  initial begin
+    output <= 0;
+  end
+
+  always @(*) begin 
+
+    output <= in[0] & in[1];
+
+  end
+
+
+endmodule
+
+
 module structural (hikingClub, basketBallTeam, going_out);
   
 
@@ -32,13 +71,12 @@ module structural (hikingClub, basketBallTeam, going_out);
   input basketBallTeam[0:1];
   output going_out;
 
+  
 
-  initial begin
-    going_out <= 0;
-  end
+  
 
-  always @(*) 
-  begin
+  gate_and myAnd()
+
     going_out <= hikingClub[0] | hikingClub[1] && basketBallTeam[0] | basketBallTeam[1];
 
   end
